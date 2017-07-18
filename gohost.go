9bb413e5e672876da6a90920ad09df28a3344062
Stan Nelson <eleniums@yahoo.com>
2017-07-18 13:19:23 -0700

    Added function to serve up http endpoint.

--- gohost.go
+++ gohost.go
@@ -1,9 +1,13 @@
 package gohost
 
 import (
+	"context"
 	"fmt"
 	"net"
+	"net/http"
 
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"github.com/rs/cors"
 	"google.golang.org/grpc"
 )
 
@@ -12,7 +16,7 @@ func ServeGRPC(server Server, grpcAddr string, opts []grpc.ServerOption) error {
 	// start listening
 	lis, err := net.Listen("tcp", grpcAddr)
 	if err != nil {
-		return fmt.Errorf("Failed to listen: %v", err)
+		return fmt.Errorf("failed to listen: %v", err)
 	}
 
 	// register server
@@ -21,7 +25,35 @@ func ServeGRPC(server Server, grpcAddr string, opts []grpc.ServerOption) error {
 
 	// start server
 	if err := grpcServer.Serve(lis); err != nil {
-		return fmt.Errorf("Failed to serve: %v", err)
+		return fmt.Errorf("failed to serve gRPC endpoint: %v", err)
+	}
+
+	return nil
+}
+
+// ServeHTTP starts an HTTP endpoint for a given server. This is a gateway pointing to a gRPC endpoint.
+func ServeHTTP(server Server, httpAddr string, grpcAddr string, enableCORS bool, opts []grpc.DialOption) error {
+	// create context
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	// register server
+	mux := runtime.NewServeMux()
+	err := server.RegisterHandler(ctx, mux, grpcAddr, opts)
+	if err != nil {
+		return fmt.Errorf("failed to register HTTP endpoint: %v", err)
+	}
+
+	// enable CORS if requested
+	if enableCORS {
+		mux := cors.AllowAll().Handler(mux)
+	}
+
+	// start server
+	err = http.ListenAndServe(httpAddr, mux)
+	if err != nil {
+		return fmt.Errorf("failed to serve HTTP endpoint: %v", err)
 	}
 
 	return nil
