b8d2f059aa636397356cb9901241fd65a4186684
Stan Nelson <eleniums@yahoo.com>
2017-08-09 18:45:48 -0700

    Added more tests to increase code coverage.

--- gohost_test.go
+++ gohost_test.go
@@ -1,6 +1,7 @@
 package gohost
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -12,6 +13,7 @@ import (
 	"github.com/eleniums/gohost/examples/test"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 
 	pb "github.com/eleniums/gohost/examples/test/proto"
 	assert "github.com/stretchr/testify/require"
@@ -67,11 +69,53 @@ func Test_ServeGRPC_EmptyGRPCAddress(t *testing.T) {
 	assert.Error(t, err)
 }
 
+func Test_ServeGRPC_FailListen(t *testing.T) {
+	// arrange
+	service := test.NewService()
+	grpcAddr := "badaddress"
+
+	// act - start the service
+	err := ServeGRPC(service, grpcAddr, nil)
+
+	// assert
+	assert.Error(t, err)
+}
+
+func Test_ServeGRPCWithTLS_Successful(t *testing.T) {
+	// arrange
+	service := test.NewService()
+	grpcAddr := getAddr(t)
+	certFile := "./testdata/test.crt"
+	keyFile := "./testdata/test.key"
+
+	expectedValue := "test"
+
+	// act - start the service
+	go ServeGRPCWithTLS(service, grpcAddr, nil, certFile, keyFile)
+
+	// make sure service has time to start
+	time.Sleep(time.Millisecond * 100)
+
+	// call the service
+	conn, err := grpc.Dial(grpcAddr, grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{InsecureSkipVerify: true})))
+	assert.NoError(t, err)
+	client := pb.NewTestServiceClient(conn)
+	req := pb.SendRequest{
+		Value: expectedValue,
+	}
+	resp, err := client.Echo(context.Background(), &req)
+
+	// assert
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+	assert.Equal(t, expectedValue, resp.Echo)
+}
+
 func Test_ServeGRPCWithTLS_NilService(t *testing.T) {
 	// arrange
 	grpcAddr := getAddr(t)
-	certFile := "certfile"
-	keyFile := "keyfile"
+	certFile := "./testdata/test.crt"
+	keyFile := "./testdata/test.key"
 
 	// act
 	err := ServeGRPCWithTLS(nil, grpcAddr, nil, certFile, keyFile)
@@ -83,8 +127,8 @@ func Test_ServeGRPCWithTLS_NilService(t *testing.T) {
 func Test_ServeGRPCWithTLS_EmptyGRPCAddress(t *testing.T) {
 	// arrange
 	service := test.NewService()
-	certFile := "certfile"
-	keyFile := "keyfile"
+	certFile := "./testdata/test.crt"
+	keyFile := "./testdata/test.key"
 
 	// act
 	err := ServeGRPCWithTLS(service, "", nil, certFile, keyFile)
@@ -97,7 +141,7 @@ func Test_ServeGRPCWithTLS_EmptyCertFile(t *testing.T) {
 	// arrange
 	service := test.NewService()
 	grpcAddr := getAddr(t)
-	keyFile := "keyfile"
+	keyFile := "./testdata/test.key"
 
 	// act
 	err := ServeGRPCWithTLS(service, grpcAddr, nil, "", keyFile)
@@ -110,10 +154,24 @@ func Test_ServeGRPCWithTLS_EmptyKeyFile(t *testing.T) {
 	// arrange
 	service := test.NewService()
 	grpcAddr := getAddr(t)
-	certFile := "certfile"
+	certFile := "./testdata/test.crt"
 
 	// act
 	err := ServeGRPCWithTLS(service, grpcAddr, nil, certFile, "")
+
+	// assert
+	assert.Error(t, err)
+}
+
+func Test_ServeGRPCWithTLS_FailListen(t *testing.T) {
+	// arrange
+	service := test.NewService()
+	grpcAddr := "badaddress"
+	certFile := "./testdata/test.crt"
+	keyFile := "./testdata/test.key"
+
+	// act - start the service
+	err := ServeGRPCWithTLS(service, grpcAddr, nil, certFile, keyFile)
 
 	// assert
 	assert.Error(t, err)
@@ -130,6 +188,41 @@ func Test_ServeHTTP_Successful(t *testing.T) {
 	// act - start the service
 	go ServeGRPC(service, grpcAddr, nil)
 	go ServeHTTP(service, httpAddr, grpcAddr, false, nil)
+
+	// make sure service has time to start
+	time.Sleep(time.Millisecond * 100)
+
+	// call the service
+	httpClient := http.Client{
+		Timeout: time.Millisecond * 500,
+	}
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://%v/v1/echo?value="+expectedValue, httpAddr), nil)
+	assert.NoError(t, err)
+	doResp, err := httpClient.Do(req)
+	assert.NoError(t, err)
+	assert.Equal(t, 200, doResp.StatusCode)
+	body, err := ioutil.ReadAll(doResp.Body)
+	assert.NoError(t, err)
+	resp := pb.EchoResponse{}
+	err = json.Unmarshal(body, &resp)
+
+	// assert
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+	assert.Equal(t, expectedValue, resp.Echo)
+}
+
+func Test_ServeHTTP_EnableCORS(t *testing.T) {
+	// arrange
+	service := test.NewService()
+	httpAddr := getAddr(t)
+	grpcAddr := getAddr(t)
+
+	expectedValue := "test"
+
+	// act - start the service
+	go ServeGRPC(service, grpcAddr, nil)
+	go ServeHTTP(service, httpAddr, grpcAddr, true, nil)
 
 	// make sure service has time to start
 	time.Sleep(time.Millisecond * 100)
@@ -190,12 +283,110 @@ func Test_ServeHTTP_EmptyGRPCAddress(t *testing.T) {
 	assert.Error(t, err)
 }
 
+func Test_ServeHTTP_FailListen(t *testing.T) {
+	// arrange
+	service := test.NewService()
+	httpAddr := "badaddress"
+	grpcAddr := getAddr(t)
+
+	// act - start the service
+	go ServeGRPC(service, grpcAddr, nil)
+	err := ServeHTTP(service, httpAddr, grpcAddr, false, nil)
+
+	// assert
+	assert.Error(t, err)
+}
+
+func Test_ServeHTTPWithTLS_Successful(t *testing.T) {
+	// arrange
+	service := test.NewService()
+	httpAddr := getAddr(t)
+	grpcAddr := getAddr(t)
+	certFile := "./testdata/test.crt"
+	keyFile := "./testdata/test.key"
+
+	expectedValue := "test"
+
+	// act - start the service
+	go ServeGRPCWithTLS(service, grpcAddr, nil, certFile, keyFile)
+	go ServeHTTPWithTLS(service, httpAddr, grpcAddr, false, nil, certFile, keyFile, true)
+
+	// make sure service has time to start
+	time.Sleep(time.Millisecond * 100)
+
+	// call the service
+	httpClient := http.Client{
+		Timeout: time.Millisecond * 500,
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
+	}
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("https://%v/v1/echo?value="+expectedValue, httpAddr), nil)
+	assert.NoError(t, err)
+	doResp, err := httpClient.Do(req)
+	assert.NoError(t, err)
+	assert.Equal(t, 200, doResp.StatusCode)
+	body, err := ioutil.ReadAll(doResp.Body)
+	assert.NoError(t, err)
+	resp := pb.EchoResponse{}
+	err = json.Unmarshal(body, &resp)
+
+	// assert
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+	assert.Equal(t, expectedValue, resp.Echo)
+}
+
+func Test_ServeHTTPWithTLS_EnableCORS(t *testing.T) {
+	// arrange
+	service := test.NewService()
+	httpAddr := getAddr(t)
+	grpcAddr := getAddr(t)
+	certFile := "./testdata/test.crt"
+	keyFile := "./testdata/test.key"
+
+	expectedValue := "test"
+
+	// act - start the service
+	go ServeGRPCWithTLS(service, grpcAddr, nil, certFile, keyFile)
+	go ServeHTTPWithTLS(service, httpAddr, grpcAddr, true, nil, certFile, keyFile, true)
+
+	// make sure service has time to start
+	time.Sleep(time.Millisecond * 100)
+
+	// call the service
+	httpClient := http.Client{
+		Timeout: time.Millisecond * 500,
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
+	}
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("https://%v/v1/echo?value="+expectedValue, httpAddr), nil)
+	assert.NoError(t, err)
+	doResp, err := httpClient.Do(req)
+	assert.NoError(t, err)
+	assert.Equal(t, 200, doResp.StatusCode)
+	body, err := ioutil.ReadAll(doResp.Body)
+	assert.NoError(t, err)
+	resp := pb.EchoResponse{}
+	err = json.Unmarshal(body, &resp)
+
+	// assert
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+	assert.Equal(t, expectedValue, resp.Echo)
+}
+
 func Test_ServeHTTPWithTLS_NilService(t *testing.T) {
 	// arrange
 	httpAddr := getAddr(t)
 	grpcAddr := getAddr(t)
-	certFile := "certfile"
-	keyFile := "keyfile"
+	certFile := "./testdata/test.crt"
+	keyFile := "./testdata/test.key"
 
 	// act
 	err := ServeHTTPWithTLS(nil, httpAddr, grpcAddr, false, nil, certFile, keyFile, false)
@@ -208,8 +399,8 @@ func Test_ServeHTTPWithTLS_EmptyHTTPAddress(t *testing.T) {
 	// arrange
 	service := test.NewService()
 	grpcAddr := getAddr(t)
-	certFile := "certfile"
-	keyFile := "keyfile"
+	certFile := "./testdata/test.crt"
+	keyFile := "./testdata/test.key"
 
 	// act
 	err := ServeHTTPWithTLS(service, "", grpcAddr, false, nil, certFile, keyFile, false)
@@ -222,8 +413,8 @@ func Test_ServeHTTPWithTLS_EmptyGRPCAddress(t *testing.T) {
 	// arrange
 	service := test.NewService()
 	httpAddr := getAddr(t)
-	certFile := "certfile"
-	keyFile := "keyfile"
+	certFile := "./testdata/test.crt"
+	keyFile := "./testdata/test.key"
 
 	// act
 	err := ServeHTTPWithTLS(service, httpAddr, "", false, nil, certFile, keyFile, false)
@@ -237,7 +428,7 @@ func Test_ServeHTTPWithTLS_EmptyCertFile(t *testing.T) {
 	service := test.NewService()
 	httpAddr := getAddr(t)
 	grpcAddr := getAddr(t)
-	keyFile := "keyfile"
+	keyFile := "./testdata/test.key"
 
 	// act
 	err := ServeHTTPWithTLS(service, httpAddr, grpcAddr, false, nil, "", keyFile, false)
@@ -251,10 +442,26 @@ func Test_ServeHTTPWithTLS_EmptyKeyFile(t *testing.T) {
 	service := test.NewService()
 	httpAddr := getAddr(t)
 	grpcAddr := getAddr(t)
-	certFile := "certfile"
+	certFile := "./testdata/test.crt"
 
 	// act
 	err := ServeHTTPWithTLS(service, httpAddr, grpcAddr, false, nil, certFile, "", false)
+
+	// assert
+	assert.Error(t, err)
+}
+
+func Test_ServeHTTPWithTLS_FailListen(t *testing.T) {
+	// arrange
+	service := test.NewService()
+	httpAddr := "badaddress"
+	grpcAddr := getAddr(t)
+	certFile := "./testdata/test.crt"
+	keyFile := "./testdata/test.key"
+
+	// act - start the service
+	go ServeGRPCWithTLS(service, grpcAddr, nil, certFile, keyFile)
+	err := ServeHTTPWithTLS(service, httpAddr, grpcAddr, false, nil, certFile, keyFile, true)
 
 	// assert
 	assert.Error(t, err)
