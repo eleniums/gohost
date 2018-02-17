67e98c948ab6609a4ed2bec58a3a2e6570915c24
Stan Nelson <eleniums@yahoo.com>
2018-02-17 07:20:18 -0800

    Cleaned up serveGRPC.

--- hoster_grpc.go
+++ hoster_grpc.go
@@ -23,7 +23,7 @@ func (h *Hoster) serveGRPC() error {
 	}
 
 	// configure server options
-	serverOpts := []grpc.ServerOption{
+	opts := []grpc.ServerOption{
 		grpc.MaxSendMsgSize(h.MaxSendMsgSize),
 		grpc.MaxRecvMsgSize(h.MaxRecvMsgSize),
 	}
@@ -31,51 +31,37 @@ func (h *Hoster) serveGRPC() error {
 	// add interceptors
 	if len(h.UnaryInterceptors) > 0 {
 		unaryInterceptorChain := grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(h.UnaryInterceptors...))
-		serverOpts = append(serverOpts, unaryInterceptorChain)
+		opts = append(opts, unaryInterceptorChain)
 	}
 	if len(h.StreamInterceptors) > 0 {
 		streamInterceptorChain := grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(h.StreamInterceptors...))
-		serverOpts = append(serverOpts, streamInterceptorChain)
+		opts = append(opts, streamInterceptorChain)
 	}
 
 	// start the gRPC endpoint
 	if h.IsTLSEnabled() {
-		return ServeGRPCWithTLS(h.grpcEndpoints, h.GRPCAddr, serverOpts, h.CertFile, h.KeyFile)
+		// create TLS credentials
+		creds, err := credentials.NewServerTLSFromFile(h.CertFile, h.KeyFile)
+		if err != nil {
+			return fmt.Errorf("failed to load TLS credentials: %v", err)
+		}
+
+		// add TLS credentials to options
+		opts = append(opts, grpc.Creds(creds))
 	}
 
-	return ServeGRPC(h.grpcEndpoints, h.GRPCAddr, serverOpts)
-}
-
-// TODO: move this code up into the above method
-// ServeGRPC starts a gRPC endpoint for the given service.
-func ServeGRPC(servers []grpcServer, addr string, opts []grpc.ServerOption) error {
 	// start listening
-	lis, err := net.Listen("tcp", addr)
+	lis, err := net.Listen("tcp", h.GRPCAddr)
 	if err != nil {
 		return fmt.Errorf("failed to listen: %v", err)
 	}
 
 	// register servers
 	grpcServer := grpc.NewServer(opts...)
-	for i := range servers {
-		servers[i](grpcServer)
+	for i := range h.grpcEndpoints {
+		h.grpcEndpoints[i](grpcServer)
 	}
 
 	// start servers
 	return grpcServer.Serve(lis)
-}
-
-// ServeGRPCWithTLS starts a gRPC endpoint for the given service with TLS enabled.
-func ServeGRPCWithTLS(servers []grpcServer, addr string, opts []grpc.ServerOption, certFile string, keyFile string) error {
-	// create TLS credentials
-	creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
-	if err != nil {
-		return fmt.Errorf("failed to load TLS credentials: %v", err)
-	}
-
-	// add TLS credentials to options
-	opts = append(opts, grpc.Creds(creds))
-
-	// start server
-	return ServeGRPC(servers, addr, opts)
 }
