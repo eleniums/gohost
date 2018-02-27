700895fc6ba605873651bd810c6b5909acf15f4c
Stan Nelson <eleniums@yahoo.com>
2018-02-26 20:12:22 -0800

    Finished grpc tests.

--- test/hoster_grpc_test.go
+++ test/hoster_grpc_test.go
@@ -104,6 +104,65 @@ func Test_Hoster_ListenAndServe_GRPC_EmptyAddress(t *testing.T) {
 	assert.Error(t, err)
 }
 
+func Test_Hoster_ListenAndServe_GRPC_InvalidAddress(t *testing.T) {
+	// arrange
+	service := test.NewService()
+
+	hoster := gohost.NewHoster()
+	hoster.GRPCAddr = "badaddress"
+	hoster.RegisterGRPCEndpoint(func(s *grpc.Server) {
+		pb.RegisterTestServiceServer(s, service)
+	})
+
+	// act - start the service
+	err := hoster.ListenAndServe()
+
+	// assert
+	assert.Error(t, err)
+}
+
+func Test_Hoster_ListenAndServe_GRPC_InvalidCertFile(t *testing.T) {
+	// arrange
+	service := test.NewService()
+	grpcAddr := getAddr(t)
+
+	hoster := gohost.NewHoster()
+	hoster.GRPCAddr = grpcAddr
+	hoster.RegisterGRPCEndpoint(func(s *grpc.Server) {
+		pb.RegisterTestServiceServer(s, service)
+	})
+
+	hoster.CertFile = "./testdata/badcert.crt"
+	hoster.KeyFile = "./testdata/test.key"
+
+	// act - start the service
+	err := hoster.ListenAndServe()
+
+	// assert
+	assert.Error(t, err)
+}
+
+func Test_Hoster_ListenAndServe_GRPC_InvalidKeyFile(t *testing.T) {
+	// arrange
+	service := test.NewService()
+	grpcAddr := getAddr(t)
+
+	hoster := gohost.NewHoster()
+	hoster.GRPCAddr = grpcAddr
+	hoster.RegisterGRPCEndpoint(func(s *grpc.Server) {
+		pb.RegisterTestServiceServer(s, service)
+	})
+
+	hoster.CertFile = "./testdata/test.crt"
+	hoster.KeyFile = "./testdata/badkey.key"
+
+	// act - start the service
+	err := hoster.ListenAndServe()
+
+	// assert
+	assert.Error(t, err)
+}
+
 func Test_Hoster_ListenAndServe_GRPC_MaxRecvMsgSize_Pass(t *testing.T) {
 	// arrange
 	service := test.NewService()
@@ -358,106 +417,3 @@ func Test_Hoster_ListenAndServe_GRPC_StreamInterceptor(t *testing.T) {
 	assert.NotNil(t, testResp)
 	assert.True(t, testResp.Success)
 }
-
-// func Test_ServeGRPC_EmptyGRPCAddress(t *testing.T) {
-// 	// arrange
-// 	service := test.NewService()
-
-// 	// act
-// 	err := ServeGRPC(service, "", nil)
-
-// 	// assert
-// 	assert.Error(t, err)
-// }
-
-// func Test_ServeGRPC_FailListen(t *testing.T) {
-// 	// arrange
-// 	service := test.NewService()
-// 	grpcAddr := "badaddress"
-
-// 	// act - start the service
-// 	err := ServeGRPC(service, grpcAddr, nil)
-
-// 	// assert
-// 	assert.Error(t, err)
-// }
-
-// func Test_ServeGRPCWithTLS_NilService(t *testing.T) {
-// 	// arrange
-// 	grpcAddr := getAddr(t)
-// 	certFile := "./testdata/test.crt"
-// 	keyFile := "./testdata/test.key"
-
-// 	// act
-// 	err := ServeGRPCWithTLS(nil, grpcAddr, nil, certFile, keyFile)
-
-// 	// assert
-// 	assert.Error(t, err)
-// }
-
-// func Test_ServeGRPCWithTLS_EmptyGRPCAddress(t *testing.T) {
-// 	// arrange
-// 	service := test.NewService()
-// 	certFile := "./testdata/test.crt"
-// 	keyFile := "./testdata/test.key"
-
-// 	// act
-// 	err := ServeGRPCWithTLS(service, "", nil, certFile, keyFile)
-
-// 	// assert
-// 	assert.Error(t, err)
-// }
-
-// func Test_ServeGRPCWithTLS_EmptyCertFile(t *testing.T) {
-// 	// arrange
-// 	service := test.NewService()
-// 	grpcAddr := getAddr(t)
-// 	keyFile := "./testdata/test.key"
-
-// 	// act
-// 	err := ServeGRPCWithTLS(service, grpcAddr, nil, "", keyFile)
-
-// 	// assert
-// 	assert.Error(t, err)
-// }
-
-// func Test_ServeGRPCWithTLS_EmptyKeyFile(t *testing.T) {
-// 	// arrange
-// 	service := test.NewService()
-// 	grpcAddr := getAddr(t)
-// 	certFile := "./testdata/test.crt"
-
-// 	// act
-// 	err := ServeGRPCWithTLS(service, grpcAddr, nil, certFile, "")
-
-// 	// assert
-// 	assert.Error(t, err)
-// }
-
-// func Test_ServeGRPCWithTLS_BadCertFile(t *testing.T) {
-// 	// arrange
-// 	service := test.NewService()
-// 	grpcAddr := getAddr(t)
-// 	certFile := "./testdata/badcert.crt"
-// 	keyFile := "./testdata/test.key"
-
-// 	// act - start the service
-// 	err := ServeGRPCWithTLS(service, grpcAddr, nil, certFile, keyFile)
-
-// 	// assert
-// 	assert.Error(t, err)
-// }
-
-// func Test_ServeGRPCWithTLS_FailListen(t *testing.T) {
-// 	// arrange
-// 	service := test.NewService()
-// 	grpcAddr := "badaddress"
-// 	certFile := "./testdata/test.crt"
-// 	keyFile := "./testdata/test.key"
-
-// 	// act - start the service
-// 	err := ServeGRPCWithTLS(service, grpcAddr, nil, certFile, keyFile)
-
-// 	// assert
-// 	assert.Error(t, err)
-// }
