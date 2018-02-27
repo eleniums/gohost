e38514e445f3702c49729d2091b48f8084b8abbd
Stan Nelson <eleniums@yahoo.com>
2018-02-26 19:58:22 -0800

    Add debug endpoint tests.

--- test/hoster_debug_test.go
+++ test/hoster_debug_test.go
@@ -12,7 +12,7 @@ import (
 	assert "github.com/stretchr/testify/require"
 )
 
-func Test_Hoster_ListenAndServe_DebugEndpoint_Pprof(t *testing.T) {
+func Test_Hoster_ListenAndServe_Debug_Pprof(t *testing.T) {
 	// arrange
 	debugAddr := getAddr(t)
 
@@ -42,7 +42,7 @@ func Test_Hoster_ListenAndServe_DebugEndpoint_Pprof(t *testing.T) {
 	assert.NotEmpty(t, body)
 }
 
-func Test_Hoster_ListenAndServe_DebugEndpoint_Vars(t *testing.T) {
+func Test_Hoster_ListenAndServe_Debug_Vars(t *testing.T) {
 	// arrange
 	debugAddr := getAddr(t)
 
@@ -70,4 +70,43 @@ func Test_Hoster_ListenAndServe_DebugEndpoint_Vars(t *testing.T) {
 	// assert
 	assert.NoError(t, err)
 	assert.NotEmpty(t, body)
+}
+
+func Test_Hoster_ListenAndServe_Debug_Disabled(t *testing.T) {
+	// arrange
+	debugAddr := getAddr(t)
+
+	hoster := gohost.NewHoster()
+
+	hoster.DebugAddr = debugAddr
+	hoster.EnableDebug = false
+
+	// act - start the service
+	go hoster.ListenAndServe()
+
+	// make sure service has time to start
+	time.Sleep(serviceStartDelay)
+
+	// call the service at the HTTP endpoint
+	httpClient := http.Client{
+		Timeout: httpClientTimeout,
+	}
+	httpReq, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://%v/debug/pprof", debugAddr), nil)
+	assert.NoError(t, err)
+	_, err = httpClient.Do(httpReq)
+	assert.Error(t, err)
+}
+
+func Test_Hoster_ListenAndServe_Debug_EmptyAddress(t *testing.T) {
+	// arrange
+	hoster := gohost.NewHoster()
+
+	hoster.DebugAddr = ""
+	hoster.EnableDebug = true
+
+	// act - start the service
+	err := hoster.ListenAndServe()
+
+	// assert
+	assert.Error(t, err)
 }
