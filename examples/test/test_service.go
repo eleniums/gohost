419103ff5c706069a156a618882a6e565687b4e5
Stan Nelson <eleniums@yahoo.com>
2018-03-05 18:05:43 -0800

    Added generate commands to test service.

--- examples/test/test_service.go
+++ examples/test/test_service.go
@@ -8,6 +8,10 @@ import (
 	pb "github.com/eleniums/gohost/examples/test/proto"
 )
 
+//go:generate protoc -I. -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc:. proto/test.proto
+//go:generate protoc -I. -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --grpc-gateway_out=logtostderr=true:. proto/test.proto
+//go:generate protoc -I. -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --proto_path=./proto --swagger_out=logtostderr=true:. proto/test.proto
+
 // Service contains the implementation for the gRPC service. It implements the interfaces for both gRPC and HTTP endpoints.
 type Service struct{}
 
